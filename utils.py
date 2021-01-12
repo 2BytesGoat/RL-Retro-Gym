346447be5e38015e2964eaa720c2dfc60e6d1345
2BytesGoat <2bytesgoat@gmail.com>
2021-01-12 18:03:19 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,21 @@
+import cv2
+from pathlib import Path
+
+video_path = Path('./data/raw/videoplayback.mp4')
+max_frames = 2000
+
+dump_path = video_path.parents[1] / 'splitted'
+dump_name = video_path.stem + '_{}.png'
+
+dump_path.mkdir(exist_ok=True)
+
+cap = cv2.VideoCapture(str(video_path))
+
+frame_nb = 0
+while cap.isOpened() and frame_nb < max_frames:
+    ret, frame = cap.read()
+    frame_path = str(dump_path / dump_name.format(frame_nb))
+    cv2.imwrite(frame_path, frame)
+    frame_nb += 1
+
+cap.release()
