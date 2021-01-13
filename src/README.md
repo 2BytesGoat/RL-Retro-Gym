db336486a46f0eb240babef365d5b2c1b5420523
2BytesGoat <2bytesgoat@gmail.com>
2021-01-13 16:07:35 +0200

    Updated scripts for creating encoder dataset

--- src/README.md
+++ src/README.md
@@ -1,6 +1,6 @@
 # Encoder requirements
 1. **High speed** - encoding time should take under 0.04s
-2. **High fidelity** - better performance than PCA **(metric TBD)**
+2. **High fidelity** - better performance than PCA **(MSE on pixels)**
 3. **Small encoding length** - this will be used as a tie-breaker between 1. and 2.
 4. **Low resource** - preferably running on RaspberryPi 3
 
@@ -8,6 +8,10 @@
 ## ** TBD **
 
 # Where to start
+1. Create game recordings using mario_kart_manual.py
+2. Format data for encoder using *./src/preparation/make_encoder_dataset.py*
+
+# Refferences
 [Principal Component Analysis - DataScienceHandbook](https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html) <- theory for how PCA works \
 [MLP Autoencoder Keras - Stackbuse](https://stackabuse.com/autoencoders-for-image-reconstruction-in-python-and-keras/) <- still PCA but done in keras and ez to modify\
 [Conv2D Autoencoder Keras - Keras.io](https://blog.keras.io/building-autoencoders-in-keras.html)
---
+++ src/preparation/make_encoder_dataset.py
@@ -0,0 +1,49 @@
+import cv2
+import random
+import argparse
+from pathlib import Path
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video_path", required=True, type=Path)
+    parser.add_argument("--max_frames", default=2000)
+    parser.add_argument("--export_paths", action='store_true')
+    parser.add_argument("--paths_split", default=0.75)
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    file_paths = dump_frames(args.video_path, args.max_frames)
+    if args.export_paths:
+        train_paths = random.choices(file_paths, k=int(len(file_paths) * args.paths_split))
+        valid_paths = list(set(file_paths) - set(train_paths))
+        export_paths(train_paths, args.video_path.parents[1] / 'train_files.txt')
+        export_paths(valid_paths, args.video_path.parents[1] / 'valid_files.txt')
+
+def dump_frames(video_path, max_frames):
+    dump_path = video_path.parents[1] / 'splitted'
+    dump_name = video_path.stem + '_{}.png'
+    dump_path.mkdir(exist_ok=True)
+
+    cap = cv2.VideoCapture(str(video_path))
+
+    frame_nb = 0
+    file_paths = []
+    while cap.isOpened() and frame_nb < max_frames:
+        ret, frame = cap.read()
+        frame_path = str(dump_path / dump_name.format(frame_nb))
+        cv2.imwrite(frame_path, frame)
+        file_paths.append(frame_path)
+        frame_nb += 1
+
+    cap.release()
+    return file_paths
+
+def export_paths(paths, dst_file):
+    with open(dst_file, mode='w') as dest_f:
+        for path in paths:
+            dest_f.write(str(path) + '\n')
+
+if __name__ == '__main__':
+    main()
