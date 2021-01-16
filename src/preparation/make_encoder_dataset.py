bce778026c882d06ed313e2b259c27c7a808b4cd
2BytesGoat <2bytesgoat@gmail.com>
2021-01-16 22:41:12 +0200

    Update make_encoder_dataset.py

--- src/preparation/make_encoder_dataset.py
+++ src/preparation/make_encoder_dataset.py
@@ -7,7 +7,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--video_path", required=True, type=Path)
     parser.add_argument("--max_frames", default=2000)
-    parser.add_argument("--skip_frames", default=2)
+    parser.add_argument("--skip_frames", default=0, type=int)
     parser.add_argument("--export_paths", action='store_true')
     parser.add_argument("--paths_split", default=0.75)
     args = parser.parse_args()
@@ -22,7 +22,7 @@ def main():
         export_paths(train_paths, args.video_path.parents[1] / 'train_files.txt')
         export_paths(valid_paths, args.video_path.parents[1] / 'valid_files.txt')
 
-def dump_frames(video_path, max_frames, skip_frames=3):
+def dump_frames(video_path, max_frames, skip_frames):
     dump_path = video_path.parents[1] / 'splitted'
     dump_name = video_path.stem + '_{}.png'
     dump_path.mkdir(exist_ok=True)
@@ -33,11 +33,12 @@ def dump_frames(video_path, max_frames, skip_frames=3):
     file_paths = []
     while cap.isOpened() and len(file_paths) < max_frames:
         ret, frame = cap.read()
-        if frame_nb % skip_frames == 0:
-            frame_path = str(dump_path / dump_name.format(frame_nb))
-            cv2.imwrite(frame_path, frame)
-            file_paths.append(frame_path)
         frame_nb += 1
+        if frame_nb <= skip_frames:
+            continue
+        frame_path = str(dump_path / dump_name.format(frame_nb))
+        cv2.imwrite(frame_path, frame)
+        file_paths.append(frame_path)
 
     cap.release()
     return file_paths
