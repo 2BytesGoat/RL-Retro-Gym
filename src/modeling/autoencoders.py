25a17cde1c83eb897b1bd07e0750d17f2ae852db
2BytesGoat <2bytesgoat@gmail.com>
2021-01-19 19:58:01 +0200

    Reverted to sigmoid for MLP

--- src/modeling/autoencoders.py
+++ src/modeling/autoencoders.py
@@ -47,7 +47,7 @@ class MLP(nn.Module):
             nn.Linear(self.latent_dim, 512),
             nn.ReLU(),
             nn.Linear(512, self.img_size),
-            nn.Tanh()
+            nn.Sigmoid()
         )
     
     def forward(self, frames):
