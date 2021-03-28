fd048e2995257fbc973ed57e6b1f586efb20fd6f
2BytesGoat <2bytesgoat@gmail.com>
2021-03-28 13:00:38 +0300

    Refactored - encoder creation

--- train_encoders.py
+++ train_encoders.py
@@ -5,15 +5,12 @@ from pathlib import Path
 from skimage.io import imsave
 
 import torch
-import torch.nn as nn
 from torch.utils.data.dataloader import DataLoader
 from torchvision.transforms import ToPILImage
 
-from piq import SSIMLoss # image quality losses
-
 from src.dataset import SuperMarioKartDataset
 from src.processing import get_transforms
-from src.modeling import autoencoders
+from src.modeling.autoencoders import get_encoder 
 
 """
 ToDos:
@@ -26,11 +23,11 @@ ToDos:
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_path", required=True, type=Path)
-    parser.add_argument("--save_path", default=None, type=Path)
-    parser.add_argument("--load_path", default=None, type=Path)
+    parser.add_argument("--data_path",  required=True, type=Path, help="Parent of train/valid txt files")
+    parser.add_argument("--save_path", default=None,  type=Path, help="Where to save models and visu")
+    parser.add_argument("--load_path", default=None,  type=Path, help="From where to load pretrained")
 
-    parser.add_argument("--config_name", required=True)
+    parser.add_argument("--config_path", required=True, type=Path, help="Encoder information")
     
     parser.add_argument("--viz_samples", default=5, type=int)
     parser.add_argument("--patience", default=10, type=int)
@@ -40,17 +37,12 @@ def parse_args():
 
 def main():
     args = parse_args()
-    config = load_json('./configs', args.config_name)
+    config = load_json(args.config_path)
     
-    if torch.cuda.is_available():
-        DEVICE = 'cuda'
-    else:
-        DEVICE = 'cpu'
-        print('CUDA is not available, using CPU instead...')
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # ===================Create data loaders=====================
-    transforms = get_transforms(roi=config['preprocessing']['roi'], 
-                                grayscale=config['preprocessing']['grayscale'])
+    transforms = get_transforms(**config['preprocessing'])
 
     train_set = SuperMarioKartDataset(
         Path(args.data_path) / 'train_files.txt', transforms=transforms
@@ -59,59 +51,41 @@ def main():
         Path(args.data_path) / 'valid_files.txt', transforms=transforms
     )
 
-    train_loader = DataLoader(train_set, batch_size=config['training']['batch_size'], num_workers=4)
-    valid_loader = DataLoader(train_set, batch_size=1, num_workers=4)
+    train_loader = DataLoader(train_set, batch_size=config['training']['batch_size'])
+    valid_loader = DataLoader(train_set, batch_size=1)
 
-    # ===================Export some val images=====================
+    # ===================Encoder outputs visualization=====================
     if not args.save_path:
-        save_path = args.data_path / (config['encoder']['type'] + '_results')
+        save_path = args.data_path / (config['encoder']['enc_type'] + '_results')
     else:
         save_path = args.save_path
     save_path.mkdir(exist_ok=True)
 
     # ===================Instantiate models=====================
-    img_shape = train_set.image_shape
-    if config['encoder']['type'] == 'pca':
-        network = autoencoders.PCA(img_shape, config['encoder']['latent_dim']).cuda()
-    elif config['encoder']['type'] == 'mlp':
-        network = autoencoders.MLP(img_shape, config['encoder']['latent_dim']).cuda()
+    input_shape = train_set.image_shape
+    network = get_encoder(input_shape=input_shape, device=DEVICE, **config['encoder'])
 
-    optimizer = torch.optim.Adam(network.parameters())
-    ssim_loss = SSIMLoss(data_range=1.)
-    mse_loss = nn.MSELoss()
-
-    load_path = args.load_path
-    if load_path and load_path.exists():
-        network.load_state_dict(torch.load(load_path / "best_encoder.pt"))
+    if args.load_path and args.load_path.exists():
+        network.load_state_dict(torch.load(args.load_path / "best_encoder.pt"))
 
     best_val_loss = 999
     cnt_bad_epocs = 0
 
-    for epoch in range(1, config['training']['n_episodes'] + 1):
+    for epoch in range(1, config['training']['epochs'] + 1):
         train_losses, valid_losses = [], []
         epc_save_path = save_path / f'epoch_{epoch}'
         epc_save_path.mkdir(exist_ok=True)
         # ===================Training=====================
         for x_t in train_loader:
-            x_t = x_t.cuda()
-
-            encoding, decoding = network(x_t)
-            step_mse = mse_loss(decoding, x_t)
-            loss = mse_loss(decoding, x_t)
-            train_losses.append(loss.item())
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            x_t = x_t.to(DEVICE)
+            train_loss = network.train(x_t)
+            train_losses.append(train_loss)
         # ===================Validation=====================
         for i, x_t in enumerate(valid_loader):
-            x_t = x_t.cuda()
+            x_t = x_t.to(DEVICE)
             
-            with torch.no_grad():
-                encoding, decoding = network(x_t)
-                step_mse = mse_loss(decoding, x_t)
-                step_ssim = 0.1 * ssim_loss(decoding, x_t)
-                loss = step_mse + step_ssim
+            valid_loss, encoding, decoding = network.evaluate(x_t)
+            valid_losses.append(valid_loss)
 
             if not epoch % 5 and i < args.viz_samples: # saving samples
                 input_image = np.array(ToPILImage()(x_t.cpu()[0]))
@@ -119,12 +93,10 @@ def main():
                 merged_image = np.concatenate((input_image, decoded_image))
                 imsave(epc_save_path / f'result_{i}.png', merged_image)
 
-            valid_losses.append(step_mse.item())
-
         avg_trn_loss = round(np.mean(train_losses), 4)
         avg_val_loss = round(np.mean(valid_losses), 4)
 
-        print(f'Epoch - {epoch} | Avg Train MSE - {avg_trn_loss} | Avg Val MSE - {avg_val_loss}')
+        print(f'Epoch - {epoch} | Avg Train loss - {avg_trn_loss} | Avg Val loss - {avg_val_loss}')
 
         # ===================Checkpointing=====================
         if best_val_loss > avg_val_loss:
@@ -140,11 +112,10 @@ def main():
             print('Network is not improving. Stopping training...') 
             break
 
-def load_json(fdir, name):
+def load_json(path):
     """
     Load json as python object
     """
-    path = Path(fdir) / "{}.json".format(name)
     if not path.exists():
         raise FileNotFoundError("Could not find json file: {}".format(path))
     with open(path, "r") as f:
